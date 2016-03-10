46c048248a225261c31adf4a09e57fc89acdb600
balu.s <balu.s@zohocorp.com>
2016-03-10 16:43:54 +0530

    Component initialisation from script is done.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,17 +1,25 @@
 import Ember from 'ember';
 import ModalManager from 'modal/utils/modal-manager';
+import CustomDialog from '../utils/custom-dialog';
 
 const { Controller, inject } = Ember;
 
 export default Controller.extend({
 
 	actions: {
-
-		showTemplateModal: function() {
-			ModalManager.show('create-folder-dialog', {
-				title: "Alert!!",
-				ok: "Create"
-			}, {'name':'priya'});
+		showDialog(){
+			CustomDialog.show( "create-folder-dialog", {
+				title: "Create Folder",
+				buttons: {
+					"Create Folder": function(){
+						alert("Created");
+					},
+					"Cancel": function(){
+						alert("Canceled");
+					}
+				},
+				dialogType: "modal"
+			});
 		}
 	}
 });
---
+++ app/utils/custom-dialog.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+import DialogComponent from '../components/custom-modal';
+
+export default Ember.Object.create({
+    show( innerLayout, dialogOptions ){
+
+        let options = $.extend({
+
+            container: Modal.__container__,
+
+            innerLayout: innerLayout
+
+        }, dialogOptions);
+
+        const dialog = new DialogComponent();
+
+        dialog.setProperties( options ).append();
+    }
+});
